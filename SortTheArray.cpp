23d4d3dad141c68f68bfa9f265030c6662049605
Yogeshwar Chaturvedi <56862396+YOG3SHWAR@users.noreply.github.com>
2022-05-24 23:55:05 +0530

    Create SortTheArray.cpp

---
+++ SortTheArray.cpp
@@ -0,0 +1,10 @@
+// https://practice.geeksforgeeks.org/problems/sort-the-array0055/1
+
+class Solution{
+    public:
+    vector<int> sortArr(vector<int>arr, int n){
+    //complete the function here
+    sort(arr.begin(), arr.end());
+    return arr;
+    }
+};
