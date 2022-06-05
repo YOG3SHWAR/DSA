16be74430741900059a436da58b699d5336b3be2
Yogeshwar Chaturvedi <56862396+YOG3SHWAR@users.noreply.github.com>
2022-06-05 10:28:30 +0530

    Update FactorialsOfLargeNumbers.cpp

--- Array/FactorialsOfLargeNumbers.cpp
+++ Array/FactorialsOfLargeNumbers.cpp
@@ -25,3 +25,32 @@ public:
         return v;
     }
 };
+
+// 2nd solution
+class Solution {  
+private:
+    void multiply(int x, vector<int> &res) {
+        int carry=0;
+        for(int i=0;i<res.size();i++){
+            int prod=res[i]*x+carry;
+            res[i]=prod%10;
+            carry=prod/10;
+        }
+        while(carry){
+            res.push_back(carry%10);
+            carry/=10;
+        }
+    }
+public:
+    vector<int> factorial(int N){
+        vector<int> res;
+        res.push_back(1);
+        
+        for(int i=2;i<=N;i++){
+            multiply(i, res);
+        }
+        
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
