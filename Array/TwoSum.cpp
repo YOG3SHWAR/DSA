d9d33c2ad6febe65654dbae7200f15196838143c
Yogeshwar Chaturvedi <56862396+YOG3SHWAR@users.noreply.github.com>
2022-06-06 16:10:10 +0530

    Create TwoSum.cpp

---
+++ Array/TwoSum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(map.find(target-nums[i])!=map.end()){
+                ans.push_back(map[target-nums[i]]);
+                ans.push_back(i);
+                break;
+            } else {
+                map[nums[i]]=i;
+            }
+        }
+        return ans;
+    }
+};
