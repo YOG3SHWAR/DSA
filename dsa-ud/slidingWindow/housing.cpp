d68b409cd40eb17e5abd9bfd47ed7812d522394f
Yogeshwar Chaturvedi <kanhay8@gmail.com>
2022-06-19 19:48:22 +0530

    housing

---
+++ dsa-ud/slidingWindow/housing.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> housing(vector<int> arr,int s) {
+	vector<vector<int>> ans;
+	int n=arr.size();
+	int i=0;
+	int j=0;
+	int sum=0;
+	while(j<n) {
+		sum+=arr[j];
+		while(sum>s && i<=j) {
+			sum-=arr[i];
+			i++;
+		}
+		if(sum==s)
+			ans.push_back({i, j});
+		j++;
+	}
+	return ans;
+}
+
+int main() {
+	int n;
+	cin>>n;
+	int input;
+	vector<int> arr;
+	int sum;
+	for(int i=0;i<n;i++) {
+		cin>>input;
+		arr.push_back(input);
+	}
+	cin>>sum;
+	for(auto i: housing(arr, sum)){
+		for(auto j:i) cout<<j<<" ";
+		cout<<endl;
+	}
+	return 0;
+}
