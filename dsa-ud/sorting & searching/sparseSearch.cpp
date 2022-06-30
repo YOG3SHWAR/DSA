6b4b2d5c61ea23ace6eedc4ec11711ecd53a884a
Yogeshwar Chaturvedi <kanhay8@gmail.com>
2022-06-30 21:30:11 +0530

    sparse search

---
+++ dsa-ud/sorting & searching/sparseSearch.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sparse_search(string arr[], int n, string search) {
+	int s=0;
+	int e=n-1;
+	int mid;
+	while(s<=e) {
+		mid=(s+e)/2;
+		if(arr[mid]==search)
+			return mid;
+		if(arr[mid]>search) {
+			e=mid-1;
+		}
+		if(arr[mid]<search) {
+			s=mid+1;
+		}
+		// update mid to point to nearest non empty string
+		int i=mid+1;
+		int j=mid-1;
+		if(arr[mid]=="") {
+			while(1) {
+				if(i>e && j<s)
+					return -1;
+				if(arr[i]!=""){
+					mid=i;
+					break;
+				}
+				if(arr[j]!="") {
+					mid=j;
+					break;
+				}
+				i++;
+				j--;
+			}
+		}
+	}
+	return -1;
+}
+
+int main(int argc, char const *argv[]) {
+	string arr[]={"ai","","","bat","","","car","cat","","","dog","e"};
+	int n=12;
+	cout<<sparse_search(arr,n,"dog");
+	return 0;
+}
